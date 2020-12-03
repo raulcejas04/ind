102bae8fdc2f8aca34bc67a0d091416646a31376
rbianchi34 <rbianchi34@gmail.com>
2020-12-03 15:18:57 -0300

    migraciones arreglo migraciones de doctrine para que creen todas las tablas de la base de 0 sin errores

--- migrations/Version20200708192032.php
+++ migrations/Version20200708192032.php
@@ -20,6 +20,7 @@ final class Version20200708192032 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE grupo (id INT AUTO_INCREMENT NOT NULL, nombre VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('CREATE TABLE grupo_user (grupo_id INT NOT NULL, user_id INT NOT NULL, INDEX IDX_E90A3F339C833003 (grupo_id), INDEX IDX_E90A3F33A76ED395 (user_id), PRIMARY KEY(grupo_id, user_id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
         $this->addSql('ALTER TABLE grupo_user ADD CONSTRAINT FK_E90A3F339C833003 FOREIGN KEY (grupo_id) REFERENCES grupo (id) ON DELETE CASCADE');
         $this->addSql('ALTER TABLE grupo_user ADD CONSTRAINT FK_E90A3F33A76ED395 FOREIGN KEY (user_id) REFERENCES user (id) ON DELETE CASCADE');
@@ -28,6 +29,7 @@ final class Version20200708192032 extends AbstractMigration
     public function down(Schema $schema) : void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE grupo_user');
+        $this->addSql('DROP TABLE grupo_user');        
+        $this->addSql('DROP TABLE grupo');
     }
 }
--- migrations/Version20200821132005.php
+++ migrations/Version20200821132005.php
@@ -20,12 +20,12 @@ final class Version20200821132005 extends AbstractMigration
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE usuario ADD nombre_img_perfil VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE TABLE usuario (id INT AUTO_INCREMENT NOT NULL, compania_id INT DEFAULT NULL, email VARCHAR(180) NOT NULL, roles JSON NOT NULL, password VARCHAR(255) NOT NULL, nombre VARCHAR(180) NOT NULL, apellido VARCHAR(180) NOT NULL, username VARCHAR(180) NOT NULL, nombre_img_perfil VARCHAR(255) DEFAULT NULL, UNIQUE INDEX UNIQ_2265B05DE7927C74 (email), UNIQUE INDEX UNIQ_2265B05DF85E0677 (username), INDEX IDX_2265B05D12A65948 (compania_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+     
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE usuario DROP nombre_img_perfil');
+        $this->addSql('DROP TABLE usuario');
     }
 }
