cf0db3926684156b636d33b123f3f0b55a75d8e8
jose talerman <augustotalerman@gmail.com>
2020-12-10 17:28:09 +0000

    Update General.php

--- src/Entity/General.php
+++ src/Entity/General.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\GeneralRepository;
 use Doctrine\ORM\Mapping as ORM;
 use App\Entity\Tipo;
+use App\Entity\Habilitacion
 
 /**
  * @ORM\Entity(repositoryClass=GeneralRepository::class)
